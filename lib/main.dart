5022912bb572a587c1945e29d6fda268b6e1120d
eswar2001 <teswar2001@yahoo.co.in>
2020-04-19 10:41:34 +0530

    added search

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:Count/country.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -26,50 +28,57 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  @override
-  String s = '';
   bool isSearching = false;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: !isSearching
-              ? Text(widget.title)
-              : TextField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.search,
-                        color: Colors.white,
-                      ),
-                      hintText: "Search county here",
-                      hintStyle: TextStyle(color: Colors.white)),
-                ),
-          actions: [
-            !isSearching
-                ? IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: () {
-                      setState(() {
-                        this.isSearching = true;
-                      });
-                    },
-                  )
-                : IconButton(
-                    icon: Icon(Icons.cancel),
-                    onPressed: () {
-                      setState(() {
+        elevation: 0.8,
+        title: !isSearching
+            ? Text('All Countries')
+            : TextField(
+                onChanged: (value) {
+                  _filterCountries(value);
+                },
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    hintText: "Search Country Here",
+                    hintStyle: TextStyle(color: Colors.white)),
+              ),
+        actions: <Widget>[
+          isSearching
+              ? IconButton(
+                  icon: Icon(Icons.cancel),
+                  onPressed: () {
+                    setState(
+                      () {
                         this.isSearching = false;
-                      });
-                    },
-                  )
-          ]),
+                        filteredCountries = countries;
+                      },
+                    );
+                  },
+                )
+              : IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    setState(
+                      () {
+                        this.isSearching = true;
+                      },
+                    );
+                  },
+                )
+        ],
+      ),
       body: Container(
         child: FutureBuilder(
           future: _getcountries(),
@@ -78,36 +87,69 @@ class _MyHomePageState extends State<MyHomePage> {
             if (snapshot.data == null) {
               return Container(child: Center(child: Text('Null')));
             } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Image.network(
-                            'https://www.countryflags.io/' +
-                                snapshot.data[index].countrycode +
-                                '/flat/64.png',
-                          ),
-                          Text(snapshot.data[index].name),
-                          Text(snapshot.data[index].totalConfirmed.toString()),
-                          FlatButton(
-                              onPressed: () {
+              return filteredCountries.length > 0
+                  ? !isSearching
+                      ? ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    Image.network(
+                                      'https://www.countryflags.io/' +
+                                          snapshot.data[index].countrycode +
+                                          '/flat/64.png',
+                                    ),
+                                    Text(snapshot.data[index].name),
+                                    Text(snapshot.data[index].totalConfirmed
+                                        .toString()),
+                                    FlatButton(
+                                        onPressed: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      DetailPage(snapshot
+                                                          .data[index])));
+                                        },
+                                        child: Text('More Data'))
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      : ListView.builder(
+                          itemCount: filteredCountries.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                              onTap: () {
                                 Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            DetailPage(snapshot.data[index])));
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        DetailPage(filteredCountries[index]),
+                                  ),
+                                );
                               },
-                              child: Text('More Data'))
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
+                              child: Card(
+                                elevation: 10,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 8),
+                                  child: Text(
+                                    filteredCountries[index].name,
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                              ),
+                            );
+                          })
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    );
             }
           },
         ),
@@ -115,8 +157,28 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  List<Country> countries = [];
+  List<Country> filteredCountries = [];
+  @override
+  void initState() {
+    _getcountries().then((data) {
+      setState(() {
+        countries = filteredCountries = data;
+      });
+    });
+    super.initState();
+  }
+
+  void _filterCountries(value) {
+    setState(() {
+      filteredCountries = countries
+          .where((country) =>
+              country.name.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
   Future<List<Country>> _getcountries() async {
-    List<Country> countries = [];
     var data = await http.get('https://api.covid19api.com/summary');
     var jsonData = json.decode(data.body);
     // print(jsonData);
@@ -138,3 +200,4 @@ class _MyHomePageState extends State<MyHomePage> {
     return countries;
   }
 }
+
